6a1b8b90d100ca9be79c289932e4d71583e38da9
maxkuniev <yoggi2004@gmail.com>
2026-04-19 00:10:40 +0300

    Minor changes. Selected commit, before the introduction of feature branch workflow

--- djangocoursera/settings.py
+++ djangocoursera/settings.py
@@ -11,22 +11,22 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 import os
-from pathlib import Path
 from dotenv import load_dotenv
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+from pathlib import Path
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-load_dotenv()
-
 # SECURITY WARNING: keep the secret key used in production secret!
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / ".env")
+
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
+if not SECRET_KEY:
+    raise ValueError("SECRET_KEY is not set!")
+
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['adore272.pythonanywhere.com', '127.0.0.1', 'localhost']
