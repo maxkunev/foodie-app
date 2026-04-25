a74aa6789c3a4d725c57e4648349e5ecae176c0f
maxkuniev <yoggi2004@gmail.com>
2026-04-26 00:03:01 +0300

    Fixed ajax function to Optimistic UI

--- static/js/recipe/favorites-ajax.js
+++ static/js/recipe/favorites-ajax.js
@@ -3,6 +3,18 @@ let favoriteForms = document.querySelectorAll(".ajax-form-feature");
 favoriteForms.forEach(function(form) {
     form.addEventListener('submit', function(event) {
         event.preventDefault();
+        let button = form.querySelector("button");
+        let classlist_origin = button.classList
+        if (button.classList.contains("btn-twitch")){
+            button.classList.remove("btn-twitch", "text-white")
+            button.classList.add("btn-warning", "text-black")
+            button.innerText = "🖤 Unfavorite";
+        }
+        else{
+            button.classList.remove("btn-warning", "text-black")
+            button.classList.add("btn-twitch", "text-white")
+            button.innerText = "🤍Favorite";
+        }
         let url = form.action; 
         let formData = new FormData(form);
         fetch(url, {
@@ -10,21 +22,23 @@ favoriteForms.forEach(function(form) {
             body: formData,
             headers: {"Accept": "application/json"}
         })
-        .then(response => response.json()) 
+        .then(response => {
+            if (!response.ok){
+                throw new Error('Something went wrong! Server has trouble.')
+                button.classList = classlist_origin;
+            };
+            return response.json()
+        }) 
         .then(data => {
             let button = form.querySelector("button");
             if ((data.user_recipe_is_favorite === true)){
-                button.classList.remove("btn-twitch", "text-white")
-                button.classList.add("btn-warning", "text-black")
-                button.innerText = "🖤 Unfavorite";
+                console.log("Favorite added success!")
             }
-            else{
-                button.classList.remove("btn-warning", "text-black")
-                button.classList.add("btn-twitch", "text-white")
-                button.innerText = "🤍Favorite";
-            }
-            console.log(data);
+            console.log(data)})
+        .catch(error => {
+            console.error("There was an error", error);
+            });
         });
     });
-});
+
    
